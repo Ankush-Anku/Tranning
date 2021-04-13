ada3af31193bca21952a237a0f1eec74ae73c03c
Ankush-Anku <ankush27apr1996@gmail.com>
2021-04-13 16:59:57 +0530

    13 April

---
+++ 84_sorting.py
@@ -0,0 +1,18 @@
+numbers = [33,55,6,77,8,9]
+print(numbers)
+numbers.sort()
+print(numbers)
+
+cities =["Hyderabad","chennai","Bengaluru","Mumbai"]
+cities.sort()
+print(cities)
+
+"""
+sorting Techniques, Suggested reads
+bubble sort
+selection sort
+insertion sort
+merge sort
+quick sort
+radix sort
+"""
